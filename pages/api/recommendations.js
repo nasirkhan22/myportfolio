577ff7941d7bc312d1b0f8cfef8a0b5132f47f87
nasirkhan22 <nasirwaheed999@gmail.com>
2023-10-23 11:18:36 +0500

    MODIFICATION IN PORTFOLIO

--- pages/api/recommendations.js
+++ pages/api/recommendations.js
@@ -5,15 +5,15 @@ const recommendationCard = [
     name: 'Sikandar Hayat',
     image: "images/sikandar.jpeg",
     designation: 'Data Scientist | Machine Learning Engineer | Python Developer',
-    view: "I had the privilege of working alongside Osama on multiple university projects, and I must say, his talent and work ethic truly stood out. Recently, we were both part of the dynamic team at JMM Technologies, where Osama's professionalism and attention to detail shone brightly. I wholeheartedly endorse Osama for any opportunity that calls for a highly skilled and dedicated individual.",
+    view: "I had the privilege of working alongside Nasir on multiple university projects, and I must say, his talent and work ethic truly stood out. Recently, we were both part of the dynamic team at JMM Technologies, where Nasir's professionalism and attention to detail shone brightly. I wholeheartedly endorse Nasir for any opportunity that calls for a highly skilled and dedicated individual.",
     linkednURL: "https://www.linkedin.com/in/sikandar-hayat-381407179/"
   },
   {
     id: 1,
-    name: 'Nasir Khan',
+    name: 'Osama javaid',
     image: "images/osama.jpg",
     designation: 'MERN | JavaScript | Tailwind | Sass | Bootstrap',
-    view: "I wholeheartedly recommend Osama as a talented React frontend developer with an incredible flair for UI/UX design. His proficiency in Node.js further enhances his capabilities, allowing him to build robust and scalable applications. Osama's attention to detail, problem-solving skills, and dedication to delivering exceptional results make him a valuable addition to any development team.",
+    view: "I highly recommend Nasir as a React frontend developer. He is a valuable asset to our team, consistently delivering high-quality work. Nasir's strong technical skills and attention to detail are commendable. His ability to tackle complex problems and deliver elegant solutions make him a standout professional in his field.",
     linkednURL: "https://www.linkedin.com/in/nasirkhan22/"
   },
   {
@@ -21,15 +21,15 @@ const recommendationCard = [
     name: 'Muhammad Ullah',
     image: "images/muhammad.jpeg",
     designation: 'MERN Stack Developer at Productbox',
-    view: "I highly recommend Osama Javed for web frontend development positions. Their expertise in ReactJS and Next.js, combined with their professionalism and dedication, make them an invaluable asset to any team.",
+    view: "I highly recommend Nasir Khan for web frontend development positions. Their expertise in ReactJS and Next.js, combined with their professionalism and dedication, make them an invaluable asset to any team.",
     linkednURL: "https://www.linkedin.com/in/muhammadullahafridi/"
   },
   {
     id: 3,
-    name: 'Adam Murphy',
-    image: "images/adam.png",
-    designation: 'GSR MERL | Grad student at UC Santa Cruz',
-    view: "Osama is a great student who is an enthusiastic learner. He is always ready to learn new skills and is a very hard worker. His leadership skills are maturing and he is in the right direction for success.",
+    name: 'Haroon Hayat',
+    image: "images/haroon.jpg",
+    designation: 'Senior Frontend developer at JMM',
+    view: "Working with Nasir has been an absolute pleasure. As a ReactJS developer, he has shown exceptional dedication and a strong commitment to learning and growth. Nasir consistently delivers high-quality code and demonstrates a deep understanding of ReactJS principles. His ability to collaborate with the team and take on new challenges is impressive. Nasir's positive attitude and eagerness to contribute have made a significant impact on our projects. He is a quick learner and adapts well to changing requirements, always striving to deliver the best solutions. I highly recommend Nasir for any ReactJS development position. He is a valuable asset to any team and I'm confident he will continue to thrive and make significant contributions in his future endeavors.",
     linkednURL: "#"
   },
 ]
--- pages/api/review.js
+++ pages/api/review.js
@@ -18,14 +18,14 @@ const review = [
         clientName: 'Jake Corry',
         clientLocation: 'Germany',
         clientSource: 'Fiverr',
-        clientReview: "Osama is extremely knowledgeable and helpful. He knows in and out of web app development . His understanding of different technologies and frameworks in the space is also very impressive. I hope I get the chance to work with him again"
+        clientReview: "Nasir is extremely knowledgeable and helpful. He knows in and out of web app development . His understanding of different technologies and frameworks in the space is also very impressive. I hope I get the chance to work with him again"
     },
     {
         id: 4,
         clientName: 'Blaise Labriola',
         clientLocation: 'France',
         clientSource: 'Fiverr',
-        clientReview: 'I have worked with Osama and he is very professional and excellent software  developer. I admire his speed of development and quality of work when given an assignment.'
+        clientReview: 'I have worked with Nasir and he is very professional and excellent software  developer. I admire his speed of development and quality of work when given an assignment.'
     },
 ]
 export default function handler(req, res) {
